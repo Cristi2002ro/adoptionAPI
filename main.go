ed1ff50a5ab27fb2fd2e925bf0c8c13dfc990f1a
Cristi2002ro <astanescu96@yahoo.com>
2023-11-29 21:06:44 +0200

    railway deploy fix

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"adoptionAPI/handlers"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 const BaseURL = "/api"
@@ -21,11 +22,21 @@ func main() {
 	http.HandleFunc(BaseURL+"/favorites/add", handlers.HandlePostFavorite)
 	http.HandleFunc(BaseURL+"/favorites", handlers.HandleGetFavorites)
 	http.HandleFunc(BaseURL+"/favorites/delete", handlers.HandleDeleteFavorite)
+	port := envPortOr("3000")
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Println("application failed to serve")
 		return
 	}
 	defer dal.GetDB().Close()
+}
+
+func envPortOr(port string) string {
+	// If `PORT` variable in environment exists, return it
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		return ":" + envPort
+	}
+	// Otherwise, return the value of `port` variable from function argument
+	return ":" + port
 }
